2f1951e46df11c9fb44ea44cdb154db0516d219f
Pavi2691 <100138977+Pavi2691@users.noreply.github.com>
2023-02-20 23:18:26 +0530

    Update performance.sh

--- .github/workflows/performance.sh
+++ .github/workflows/performance.sh
@@ -3,11 +3,6 @@ cd $workspace/ops
 mkdir -p Report/Jmeter/ 
 cp -R $workspace/reports $workspace/ops/Report/Jmeter/
 cd $workspace/ops/Report/Jmeter
+rm -r *.zip
 zip -r JMeterReport_`date +"%Y%m%d%H%M%S"` reports
 rm -r reports
-git pull
-git config --global user.email "$committerEmail"
-git config --global user.name "$committerName"
-git add .
-git commit -m "Jmeter Report"
-git push
